68628b157db8edea08b4666ac8ad29466c785010
Brijesh <patelbrijesh333@gmail.com>
2018-06-12 21:51:41 +0530

    initial ngrx store actions

---
+++ src/actions/tutorial.actions.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Action } from '@ngrx/store';
+import { Tutorial } from './../models/tutorial.model';
+
+export const ADD_TUTORIAL = '[TUTORIAL] Add'
+export const REMOVE_TUTORIAL='[TUTORIAL] Remove'
+
+export class AddTutorials implements Action{
+    readonly type = ADD_TUTORIAL
+
+    constructor(public payload: Tutorial){
+
+    }
+}
+
+export class RemoveTutorials implements Action{
+    readonly type = REMOVE_TUTORIAL
+
+    constructor(public payload: number){
+
+    }
+}
